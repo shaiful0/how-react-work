e1c86d50ed386e99029141fcfd773aa0571ba987
sharifulislam <shariful0401@gmail.com>
2022-03-21 00:52:34 +0600

    add something

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Device from './component/Device/Device'; 
+import Tablet from './component/Tablet/Tablet';
 import Watch from './component/Watch/Watch';
 function App() {
   return (
     <div className="App">
        <Device name="uphone" price="2000"></Device>
       <Watch></Watch>
+      <Tablet></Tablet>
     </div>
   );
 }
